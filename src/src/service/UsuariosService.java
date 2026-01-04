0e44a6d215a61ff9e21fb53fb16becba61f2c203
Camila Bueno <camibuenorivera2005@gmail.com>
2026-01-04 12:48:27 -0500

    Remove temporary comment from UsuariosService
    Removed a comment indicating the current testing status of the connection and existence check.

--- src/src/service/UsuariosService.java
+++ src/src/service/UsuariosService.java
@@ -23,7 +23,7 @@ public class UsuariosService {
             ResultSet rs = ps.executeQuery();
 
             if (rs.next()) {
-                // ⚠️ por ahora SOLO probamos conexión y existencia
+                
                 return true;
             }
 
